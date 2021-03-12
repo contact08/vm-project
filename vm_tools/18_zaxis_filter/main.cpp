802866110ccbfbb4e629261626d374ee1e11a676
contact08 <contact08@bit5.net>
2021-03-12 09:11:10 +0900

    zfilter temporary fixed

--- vm_tools/18_zaxis_filter/main.cpp
+++ vm_tools/18_zaxis_filter/main.cpp
@@ -34,111 +34,6 @@ void bye(void)
 	destroyAllWindows();
 }
 
-typedef struct {
-	int	cols;
-	int	rows;
-	uchar	*z_count;
-} ZCOUNT;
-
-static uchar *allocate_zcount_map(Mat mat)
-{
-	uchar	*ptr = (uchar*)calloc((mat.cols*mat.rows)/8, 1);
-	return ptr;
-}
-
-static int read_zcount_map(ZCOUNT *zc, int x, int y)
-{
-	uchar	*ptr = zc->z_count + zc->cols*y + x;
-	return *ptr;
-}
-
-static void add_zcount_map(ZCOUNT *zc, int x, int y)
-{
-	uchar	*ptr = zc->z_count + zc->cols*y + x;
-	++*ptr;
-}
-
-unsigned impose_with_z_filter(Mat colmat, Mat fdepth, int near_cm, int far_cm)
-{
-	int	x, y, cm;
-	unsigned	npoints = 0;
-	float	*fptr = (float*)fdepth.data;
-	uchar	*z_count = allocate_zcount_map(colmat);
-	ZCOUNT	zc;
-
-	zc.rows = colmat.rows;
-	zc.cols = colmat.cols;
-	zc.z_count = z_count;
-
-	if (z_count == NULL) {
-		printf("failed to allocate z_count\n");
-		exit(1);
-	}
-	memset(count_cm, 0, maxcm*sizeof(float));
-	for (y = 0 ;  y < colmat.rows ; y++) {
-		for (x = 0 ;  x < colmat.cols ; x++) {
-			cm = depth2cm(*fptr++);
-			if (cm >= maxcm || cm <= 0) {
-				colmat.at<Vec3b>(y, x)[0] = 0;
-				colmat.at<Vec3b>(y, x)[1] = 0;
-				colmat.at<Vec3b>(y, x)[2] = 0;
-				continue;
-			}
-			if (cm >= far_cm || cm < near_cm) {
-				colmat.at<Vec3b>(y, x)[0] = 0;
-				colmat.at<Vec3b>(y, x)[1] = 0;
-				colmat.at<Vec3b>(y, x)[2] = 0;
-				continue;
-			}
-			++npoints;
-			++count_cm[cm];
-			add_zcount_map(&zc, x>>3, y>>3);
-		}
-	}
-	return npoints;
-#if 0	
-	fptr = (float*)fdepth.data;
-	for (y = 0 ;  y < colmat.rows ; y++) {
-		for (x = 0 ;  x < colmat.cols ; x++) {
-			cm = depth2cm(*fptr++);
-			if (cm >= maxcm || cm <= 0) {
-				colmat.at<Vec3b>(y, x)[0] = 0;
-				colmat.at<Vec3b>(y, x)[1] = 0;
-				colmat.at<Vec3b>(y, x)[2] = 0;
-				continue;
-			}
-			int n = read_zcount_map(&zc, x>>3, y>>3);
-			if (n < 25 || n > 80) {
-				colmat.at<Vec3b>(y, x)[0] = 0;
-				colmat.at<Vec3b>(y, x)[1] = 0;
-				colmat.at<Vec3b>(y, x)[2] = 0;
-				continue;
-			}
-			//colmat.at<Vec3b>(y, x)[0] = blue[cm];
-			//colmat.at<Vec3b>(y, x)[1] = green[cm];
-			//colmat.at<Vec3b>(y, x)[2] = red[cm];
-		}
-	}
-#else
-	for (y = 0 ;  y < colmat.rows ; y+=8) {
-		for (x = 0 ;  x < colmat.cols ; x+=8) {
-			int n = read_zcount_map(&zc, x>>3, y>>3);
-			if (n < 25 || n > 80) {
-				for (int j = y ; j < y+8 ; j++) {
-					for (int i = x ; i < x+8 ; i++) {
-						colmat.at<Vec3b>(j, i)[0] = 0;
-						colmat.at<Vec3b>(j, i)[1] = 0;
-						colmat.at<Vec3b>(j, i)[2] = 0;
-					}
-				}
-			}
-		}
-	}
-#endif	
-	free(z_count);
-	return npoints;
-}
-
 void z_filter(Mat edge, Mat fdepth, int near_cm, int far_cm)
 {
 	int	x, y, cm;
@@ -157,8 +52,9 @@ void z_filter(Mat edge, Mat fdepth, int near_cm, int far_cm)
 	}
 }
 
-void detect_line(Mat edge, Mat line_mat, unsigned nlines)
+void detect_line(Mat edge, Mat line_mat, unsigned nlines, double mindeg, int maxdeg)
 {
+	double	degree, slope;
 #if 1	
 	vector<Vec4i> linesP;
 	HoughLinesP(edge, linesP, 1, CV_PI/180, 30, 200, 80);		// SAKUDAIRA
@@ -170,8 +66,13 @@ void detect_line(Mat edge, Mat line_mat, unsigned nlines)
 	}
 #endif
 	if (nlines > linesP.size()) nlines = linesP.size();
+	nlines = linesP.size();
 	for(size_t i = 0; i < nlines ; i++) {
         	Vec4i l = linesP[i];
+		slope = (double)(l[3]-l[1])/(double)(l[2]-l[0]);
+		degree = R2D(atan(slope));
+		//printf("%d: %3.2lf\n", i, degree);
+		if (degree < mindeg || degree > maxdeg) continue;
         	line(line_mat, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255,255,0), 1, LINE_AA);
 	}
 #else
@@ -191,6 +92,12 @@ void detect_line(Mat edge, Mat line_mat, unsigned nlines)
         line(line_mat, pt1, pt2, Scalar(0,0,255), 1, LINE_AA);
     }
 #endif	
+}
+
+void put_axes(Mat mat, int x, int y)
+{
+	line(mat, Point(x, y), Point(x+32, y),Scalar(0,255,255),1);
+	line(mat, Point(x, y), Point(x, y+32),Scalar(0,255,255),1);
 }
 
 int main(int argc, char **argv)
@@ -246,17 +153,17 @@ loop:
 		goto loop;
 	}
 	cvtColor(rmat, colmat, cv::COLOR_GRAY2RGB);
-	//npoints = impose_with_z_filter(colmat, fdepth, near_cm, far_cm);
 	npoints = 0;
 
 	cvtColor(colmat, edge, cv::COLOR_RGB2GRAY);
-	Canny(edge, edge, 64, 200);
+	Canny(edge, edge, 48, 96);
 	imshow("original", rmat);
 	imshow("edge", edge);
 	z_filter(edge, fdepth, near_cm, far_cm);
-	detect_line(edge, line_mat, 10);
-	mat_printf(line_mat, 4, 20, "npoints:%u", npoints);
-	mat_printf(line_mat, 4, 40, "near:%4u far:%4u", near_cm, far_cm);
+	detect_line(edge, line_mat, 10, 10.0, 14.0);
+	mat_printf(line_mat, 8, 20, "npoints:%u", npoints);
+	mat_printf(line_mat, 8, 40, "near:%4u far:%4u", near_cm, far_cm);
+	put_axes(line_mat, 4, 4);
 	imshow("lines", line_mat);
 	cmd[0] = ch = waitKey(30);
 	cmd[1] = '\0';
